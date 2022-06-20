ce9d2e97424ecea4f0d40d11ea9ac4f777c72733
Joshua Luvene <JCLuvene@student.fullsail.edu>
2022-06-20 11:27:35 -0400

    Need to check why this doesn't have the button factory

--- 1.5Lab/Processor.h
+++ 1.5Lab/Processor.h
@@ -2,6 +2,23 @@
 #include "cMain.h"
 class Processor
 {
+private:
+	static Processor* calc;
+	int baseNumber;
+	Processor()//empty constructor because we don't want anyone to use this
+	{
 
+	}
+public:
+	static Processor* getInstance();
+	void setBaseNumber(int inputNum);
+
+
+	Processor(Processor& otherProcessor) = delete; //copy constructor deletes because we don't want anyone to use this
+	void operator =(const Processor& otherProcessor) = delete;//assignment constructor deletes because we don't want anyone to use this
+
+	std::string getHexadecimal();
+	std::string getBianary();
+	std::string getDecimal();
 };
 
