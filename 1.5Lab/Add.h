21809e33e0ede3dadf61bdaf3153c263e3b8239f
Joshua Luvene <JCLuvene@student.fullsail.edu>
2022-06-24 13:00:48 -0400

    Both sets of unit tests are completed and pass

--- 1.5Lab/Add.h
+++ 1.5Lab/Add.h
@@ -6,7 +6,7 @@ public:
 
 public:
 	int numFirst, numSecond;
-	double modCommand(int num1, int num2)
+	static double modCommand(int num1, int num2)
 	{
 		double answer = num1 + num2;
 		return answer;
--- 1.5Lab/Multiply.h
+++ 1.5Lab/Multiply.h
@@ -4,7 +4,7 @@ class Multiply : public IBaseCommand
 {
 public:
 	int numFirst, numSecond;
-	double modCommand(int num1, int num2)
+	static double modCommand(int num1, int num2)
 	{
 		double answer = num1 * num2;
 		return answer;
